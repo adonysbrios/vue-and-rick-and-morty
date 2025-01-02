590fb26b2640185bcb16378910b31cf05f0d95f9
Adonys B. Rios <140138535+adonysbrios@users.noreply.github.com>
2025-01-01 22:33:48 -0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
   <footer>
     <p>Made by Adonys Barzaga Rios. (Education Purposes)</p>
     <a href="mailto:adonysbarzagarios@gmail.com">Contact me</a>
-    </br>
+    <br/>
     <a href="https://github.com/adonysbrios/vue-and-rick-and-morty"> GitHub repository </a>
   </footer>
 </template>
